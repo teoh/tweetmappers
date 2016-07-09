ec2db3e0f52df6b85f4e01d28576091808fab341
Bili Sun <bili.a.sun@gmail.com>
2016-07-09 14:06:40 -0700

    Now prints tweets from a string.

--- streaming.py
+++ streaming.py
@@ -1,20 +1,28 @@
 import tweepy
 import json
+import pprint
 
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 
-with open('/Users/bilisun/tweetmappers/credentials.json') as credentials_file:
-    credentials = json.loads(credentials_file)
+with open('./credentials.json') as credentials_file:
+    credentials = json.load(credentials_file)
     consumer_key, consumer_secret = credentials['consumer_key'], credentials['consumer_secret']
     access_token, access_secret = credentials['access_token'], credentials['access_secret']
 
-print consumer_key
-print consumer_secret
-print access_token
-print access_secret
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+tweet_api = tweepy.API(auth)
 
-#auth = OAuthHandler(consumer_key, consumer_secret)
-#auth.set_access_token(access_token, access_secret)
-#tweet_api = tweepy.API(auth)
+class PrintTweetsListener(StreamListener):
+    def on_data(self, data):
+        print json.dumps(json.loads(data), indent=4)
+        return True
+
+    def on_error(self, error):
+        print json.dumps(json.loads(error), indent=4)
+        return True
+
+stream = Stream(auth, PrintTweetsListener())
+stream.filter(track=['hillary', 'clinton', 'donald', 'trump'])
