8ab6d7e3b3aa203584bfbd018001078c20655103
Cesar Ilharco Magalhaes <cesar.ilharco@gmail.com>
2016-07-10 02:44:20 -0700

    Stripping non ascii characters

--- backend/sentiment_classifier.py
+++ backend/sentiment_classifier.py
@@ -2,5 +2,9 @@ from repustate import Client
 
 repustate_client = Client(api_key='0acfbe5d0224e513e3afed3cdcb49ffbb900ceb1', version='v3')
 
+def strip_non_ascii(text):
+	new_text = [c for c in text if ord(c) < 128]
+	return ''.join(new_text)
+
 def get_sentiment(text):
-    return repustate_client.sentiment(text)['score']
+    return repustate_client.sentiment(strip_non_ascii(text))['score']
