0d013d0b5f5822fc104b1a30598a7dc0e93dc07c
Cesar Ilharco Magalhaes <cesar.ilharco@gmail.com>
2016-07-09 21:36:12 -0700

    Handling tweet negation

--- backend/classifier/naivebayes/cleaning.py
+++ backend/classifier/naivebayes/cleaning.py
@@ -4,13 +4,30 @@ from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 
 
+negation_words = {'hardly', 'lack', 'lacking', 'lacks', 'neither', 'nor', 'never', 'no', 'nobody', 'none', 'nothing', 'nowhere', 'not', 'without', 'aint', 'cant', 'cannot', 'darent', 'dont', 'doesnt', 'didnt', 'hadnt', 'hasnt', 'havent', 'havnt', 'isnt', 'mightnt', 'mustnt', 'neednt', 'oughtnt', 'shant', 'shouldnt', 'wasnt', 'wouldnt'}
+
+def handle_negation(tokens):
+	negate = False
+	treated_tokens = []
+	for token in tokens:
+		if token in negation_words:
+			negate = True
+		elif negate:
+			treated_tokens.append('not_' + token)
+		elif re.search("[.\?!,]", token):
+			negate = False
+	return treated_tokens
+
+
 def get_legit_tokens(tweet_str,
 					tknzr,
 					legit_word_re,
 					stopword_list):
 	raw_tokens = tknzr.tokenize(tweet_str)
 	stemmer = SnowballStemmer("english")
-	return [stemmer.stem(re.sub(r"""[^a-z\-]""",'',w)) for w in raw_tokens if legit_word_re.match(w) and w not in stopword_list]
+	legit_tokens = [stemmer.stem(re.sub(r"""[^a-z\-.!\?,]""",'',w)) for w in raw_tokens if legit_word_re.match(w)]
+	treated_tokens = handle_negation(legit_tokens)
+	return [stemmer.stem(re.sub(r"""[.!\?,]""",'',w)) for w in treated_tokens]
 
 
 def batch_get_legit_tokens(tweet_str_list):
